1f3f5099100dff4e9db45276ebb43d9a4919d929
Hareesh <rhareesh@zeomega.com>
2014-09-10 11:24:22 +0530

    commit

--- sqlalchemy/Member.py
+++ sqlalchemy/Member.py
@@ -9,29 +9,56 @@ from sqlalchemy import MetaData
 from sqlalchemy import Table
 
 Base = declarative_base()
+
+##
+class code_enc_type(Base):
+    __tablename__ = 'code_enc_type'
+    code_enc_type_idn  = Column(Integer, primary_key=True)
+    enc_typde_cd = Column(String(5), nullable=False)    
+
+##
+class code_gender(Base):
+    __tablename__ = 'code_gender'
+    code_gender_idn = Column(Integer, primary_key=True)
+    gender_cd = Column(String(1), nullable=False)    
+
+##
+class code_ident_type(Base):
+    __tablename__ = 'code_ident_type'
+    code_identtype_idn = Column(Integer, primary_key=True)
+    ident_type_cd = Column(String(5), nullable=False)    
+
+##
 class Member(Base):
     __tablename__ = 'Member'
     member_id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
- 
+    code_gender_idn = Column(Integer, ForeignKey('code_gender.code_gender_idn'))  
+    gender_relation = relationship(code_gender)
+     
+## 
 class Episode(Base):
     __tablename__ = 'Episode'
     id = Column(Integer, primary_key=True)
     member_id = Column(Integer, ForeignKey('Member.member_id'))
-    cert = Column(String(250), nullable=False)
-    Member = relationship(Member)
+    cert = Column(String(250), nullable=False)    	
+    code_enc_type_idn = Column(Integer, ForeignKey('code_enc_type.code_enc_type_idn'))  
+    enc_type = relationship(code_enc_type)
     member = relationship("Member",backref = backref('episodes',order_by =id))
- 
+## 
 class Identification(Base):
     __tablename__ = 'Identification'
     id = Column(Integer, primary_key=True)
     number = Column(String(250), nullable=False) 
     member_id = Column(Integer, ForeignKey('Member.member_id'))
     member = relationship("Member",backref = backref('identifications',order_by =id))
-
+    code_identtype_idn = Column(Integer, ForeignKey('code_ident_type.code_identtype_idn'))  
+    enc_type = relationship(code_ident_type)
+##
 engine=create_engine('sqlite:///:memory:',echo=True)
 Base.metadata.create_all(engine)
 
+##
 mbr_list = [(1,'tuttu',['1','2','3'] ,['fghh','zxdfsdfqwq']),(2,'mittu',['5','6','7'],['xyz12','asdasd1213'] ),(3,'chittu',['10','11'],['hhh','jjjj'])]
 DBSession = sessionmaker(bind = engine) 
 session = DBSession()
@@ -52,7 +79,6 @@ session.commit()
 mbr  = session.query(Member).filter(Member.name == 'mittu')
 print mbr
 mbr.name = 'xyz'
-print '----------------------'
 session.commit()
 for mbr in session.query(Member.name.label('Member_Name')).join(Episode,Member.member_id == Episode.member_id).filter(Episode.id ==1):
     print mbr.Member_Name
