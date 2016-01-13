f14d5d46c05c0b4c13938c83db36a84e512e183a
pkumkum <pkumkum@zeomega.com>
2016-01-13 17:06:44 +0530

    Update claimquery.py

--- spark_pgms/claimquery.py
+++ spark_pgms/claimquery.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 import datetime
 from pyspark.sql import SQLContext, Row
 
-#test comment
+#test comment1
 sc = SparkContext(appName="claimCount")
 sqlContext = SQLContext(sc)
 lines = sc.textFile("/data/claim_data/claimdata.txt")
